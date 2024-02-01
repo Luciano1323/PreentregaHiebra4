e9233bf98b4eced6158c6d7b6b1323fbe05c21d6
Luciano1323 <luhiebra@gmail.com>
2024-02-01 17:32:59 -0300

    ItemCount

--- src/Compounents/ItemListContainer.jsx
+++ src/Compounents/ItemListContainer.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import ItemDetail from './ItemDetailContainer';
+import ItemDetail from './ItemDetail';
 import { useParams } from 'react-router-dom';
 import ItemCount from './ItemCount';
 import coffesData from '../Data/Coffes';
